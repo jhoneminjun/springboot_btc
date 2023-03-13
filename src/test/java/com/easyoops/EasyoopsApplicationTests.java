d331f13480cb3303561dfb69ce7cea2a6e6835c6
user <main134679@gmail.com>
2023-03-13 12:24:59 +0900

    commit message

--- src/test/java/com/easyoops/EasyoopsApplicationTests.java
+++ src/test/java/com/easyoops/EasyoopsApplicationTests.java
@@ -12,4 +12,21 @@ class EasyoopsApplicationTests {
 	void contextLoads() {
 	}
 
+	void method2(){
+
+	}
+	void method3(){
+
+	}
+	void method4(){
+
+	}
+	void method5(){
+
+	}
+	void method6(){
+
+	}
+
+
 }
